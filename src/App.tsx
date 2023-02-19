3d70508b6cc272bbaae5a1ef2b3b2ac4043e3415
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-02-19 21:41:37 +0100

    feat: Starter and Setup code

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Hello React and Typescript World</h1>
+  )
+}
+
+export default App
